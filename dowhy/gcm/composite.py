5763413c5fcf28c10866149a3271d0b159fb6290
Arne De Temmerman <arne.detemmerman@kuleuven.be>
2025-08-19 09:29:00 +0200

    Move aggregation mechanism call inside constructor

--- dowhy/gcm/composite.py
+++ dowhy/gcm/composite.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import networkx as nx
 import numpy as np
 from dowhy.gcm.causal_models import (
@@ -9,18 +10,21 @@ from dowhy.gcm.defined_causal_mechanisms import (
     AggregationMechanism,
     DefinedStochasticModel,
 )
-
+from collections import KeysView
 
 class StructuralCausalModelComposite(ProbabilisticCausalModel):
     def __init__(
         self,
         models: list[ProbabilisticCausalModel],
-        links: dict[tuple[dict[str], str, str], AggregationMechanism] = {},
+        links: dict[tuple[dict[str], str, str], Callable] = {},
     ):
-        self.graph = self._graph(models, links)
+        self.models = models
+        self.links = links
 
-        for (u, v, aggregation_column), mechanism in links.items():
-            self.set_causal_mechanism(v, mechanism=mechanism)
+        self.graph = self._graph(models, links.keys())
+
+        for (u, v, aggregation_column), callable in links.items():
+            self.set_causal_mechanism(v, mechanism=AggregationMechanism(callable))
             self.graph.nodes[v][PARENTS_DURING_FIT] = [aggregation_column] + sorted(
                 list(u)
             )
@@ -31,17 +35,17 @@ class StructuralCausalModelComposite(ProbabilisticCausalModel):
                 )
                 self.graph.nodes[aggregation_column][PARENTS_DURING_FIT] = []
 
-    def _graph(self, models, links: list[tuple[str, str]]):
+    def _graph(self, models, links_keys: KeysView[tuple[dict[str],str,str]]):
         total_graph = models[0].graph
         for i in range(1, len(models)):
             total_graph = nx.compose(total_graph, H=models[i].graph)
-        for u, v, aggregation in links:
+        for u, v, aggregation_key in links_keys:
             for ui in u:
                 total_graph.add_edge(ui, v, time_lag=(0,))
-            total_graph.add_edge(aggregation, v, time_lag=(0,))
+            total_graph.add_edge(aggregation_key, v, time_lag=(0,))
 
         return total_graph
 
     def clone(self):
         """Clones the causal model, but keeps causal mechanisms untrained."""
-        return StructuralCausalModelComposite([model.clone() for model in self.models])
+        return StructuralCausalModelComposite([model.clone() for model in self.models],links=self.links)
